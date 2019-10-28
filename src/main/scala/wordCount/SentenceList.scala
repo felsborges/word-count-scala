be961713b9aeebdc6189869f7354fa6cfde13730
Joice Preuss Cardoso <joice.cardoso@dbserver.com.br>
2019-10-28 12:04:47 -0300

    Cria classe SentenceList

--- src/main/scala/wordCount/SentenceList.scala
+++ src/main/scala/wordCount/SentenceList.scala
@@ -1,22 +1,33 @@
 package wordCount
+import play.api.libs.json._
 
-object SentenceList {
+object Main {
     def main(args: Array[String]) {
-      
-        SentenceList.sentences
+        val sentence = new SentenceList("""[{"id": 0, "language": "pt", "text": "apples"}]""")
+
+        sentence.sentences
         //SentenceList.get(1)
-        SentenceList.freq("Texto 1")
-        SentenceList.freq("Texto 2", 1)
+        sentence.freq("Texto 1")
+        sentence.freq("Texto 2", 1)
     }
-    //Lista de listas de palavras
+}
+
+class SentenceList (doc:String) {
+    val docList = doc
+
+     //Lista de listas de palavras
     def sentences() = println("sentences")
     //e-nesima sentença
     def get(n: Int) : String = {
-        var listTest = List("lemon", "apples", "oranges", "bananas")
-        listTest(n)
+        val listTest = this.docList
+        val parsed = Json.parse(listTest)
+        (parsed \ n \ "text").as[String]
         }
     //número total de ocorrências de W
     def freq(w: String) = println("freq (1)")
     //número de ocorrências de w na sentença
     def freq(w: String, s: Int) = println("freq (2)")
+
 }
+
+
--- src/test/scala/SentenceListSpec.scala
+++ src/test/scala/SentenceListSpec.scala
@@ -7,6 +7,14 @@ class SentenceListSpec extends FunSuite with DiagrammedAssertions {
     }*/
 
     test("Dado um inteiro Deve retornar uma sentença") {
-        assert(SentenceList.get(1) == "apples")
+        
+        val sentence = new SentenceList("""[{"id": 0, "language": "pt", "text": "apples"}]""")
+        assert(sentence.get(0) == "apples")    
+    }
+
+    test("Dado um inteiro maior que 0 Deve retornar uma sentença") {
+        
+        val sentence = new SentenceList("""[{"id": 0, "language": "pt", "text": "apples"},{"id": 1, "language": "pt", "text": "banana"}]""")
+        assert(sentence.get(1) == "banana")    
     }
 }
